52cf8b2c9fe6124592fbcc2cb486a7d7b2f919a9
Nathan <ribeiro.nathan2002@gmail.com>
2025-02-24 09:33:53 -0300

    Realizado as configurações do projeto farmacia react

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,24 @@
+function Home() {
+    return (
+        <>
+            <div className="w-screen flex justify-center">
+                <div>
+                    <div className="max-w-7xl flex flex-col items-center">
+                        <h2>Seja Bem Vindos</h2>
+                        <p> ** </p>
+                    </div>
+
+                    <div className="max-w-7xl flex flex-col items-center">
+                        <img
+                            src=" "
+                            alt="Imagem da Página Home"
+                            width="400px"
+                        />
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Home
